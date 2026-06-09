3f9568844a5c679bb742a4586e10b78157f56cbc
WINKEL LAURA <50804919@ort>
2026-06-09 16:02:07 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -21,11 +21,56 @@ DARIO       |       50%
 */
 
 // COMPLETAR: Implementar la función calcularTotal que reciba el id del artista, la cantidad de entradas y un código de descuento (opcional) y devuelva el precio total a pagar en patacones.
+function calcularTotal(nombre: string, cantidadNum: number, codigoDescuento: string): number { 
+
+  let precio: number = 0;
+
+  if (nombre === "sabrina"){
+    precio = 1000 * cantidadNum;
+  }
+  else if (nombre === "kgatlw"){
+    precio = 700 * cantidadNum;
+  }
+  else if (nombre === "lali"){
+    precio = 500 * cantidadNum;
+  }
+  else if (nombre === "magdalena"){
+    precio = 600 * cantidadNum;
+  }
+  else if (nombre === "viagra"){
+    precio = 400 * cantidadNum;
+  }
+  else if (nombre === "dillom"){
+    precio = 350 * cantidadNum;
+  }
+  else if (nombre === "marilina"){
+    precio = 200 * cantidadNum;
+  }
+  else if (nombre === "mugre"){
+    precio = 150 * cantidadNum;
+  } 
+ 
+ 
+ if (codigoDescuento === "TIC10"){
+  precio = precio * 10 / 100
+ }
+
+ if (codigoDescuento === "TIC20"){
+  precio = precio * 20 / 100
+ }
+
+ if (codigoDescuento === "DARIO"){
+  precio = precio * 50 / 100
+ }
+
+return 0;
+}
 
 cuandoPasa('seleccionarArtista', ({ id, cantidad, codigoDescuento }) => {
   let cantidadNum: number = Number(cantidad);
   let precio: number = calcularTotal(id, cantidadNum, codigoDescuento);
   enviarAlFrontend('mostrarPrecio', precio);
 });
+
 
 iniciar();
