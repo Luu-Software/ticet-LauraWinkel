68e9842afcc07bb9d8ed5263d20d47f170d390ac
LauraWinkel <laurawinkel10@gmail.com>
2026-06-10 14:05:01 -0300

    Hize la consigna manija

--- src/manija.ts
+++ src/manija.ts
@@ -1,5 +1,11 @@
 export function generarLorem(n: number): string {
   // COMPLETAR: Implementar la función generarLorem que reciba un número n y devuelva una cadena de texto con la frase "Lorem Ipsum" repetida n veces, separada por espacios. Por ejemplo, si n es 3, la función debería devolver "Lorem Ipsum Lorem Ipsum Lorem Ipsum".
-}
+    if (n === 1) {
+      return "Lorem Ipsum"; // caso base
+    } else {
+      return "Lorem Ipsum " + generarLorem(n - 1); // llamada recursiva, = cada vez va sumando uno hasta que n = 1
+      // n = 5 --- Lorem Ipsum + n = 4 --- Lorem Ipsum Lorem Ipsum + n = 4 .....
+    }
+  }
 
 console.log(generarLorem(5));
